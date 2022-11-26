799890016a9fa3456265da1913217a5b8b5e064b
suvojit Chatterjee <suvojitchatterjee84@gmail.com>
2022-11-26 12:10:04 +0530

    pw classes added

--- pw/skills/array_video2.java
+++ pw/skills/array_video2.java
@@ -19,20 +19,25 @@ public class array_video2 {
         System.out.println("Original Array");
         printArray(arr);
 
-        int [] arr_2 = arr;           //shallow copy
+/*        int [] arr_2 = arr;           //shallow copy
         System.out.println("Copied array");
-        printArray(arr_2);
+        printArray(arr_2);  */
 
-        arr_2[0]=0;
+/*        arr_2[0]=0;
         arr_2[1]=0;
         System.out.println("original array after changing copied array");    //you will se that the original array is also changed
         printArray(arr);
         System.out.println("Copied  array after changing copied array");
+        printArray(arr_2);          */
+
+        int [] arr_2 = arr.clone();    //*********************************imp
+            printArray(arr_2);
+        arr_2[0]=0;
+        arr_2[1]=0;
+        System.out.println("original array after changing copied array");
+        printArray(arr);
+        System.out.println("Copied  array after changing copied array");            //in this clone only the copied array will change
         printArray(arr_2);
-
-
-
-
 
 
 
