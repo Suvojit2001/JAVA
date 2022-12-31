e5a2bf959de8aba9ecb3627b5ffd5e627d62e716
suvojit Chatterjee <suvojitchatterjee84@gmail.com>
2022-12-31 17:40:27 +0530

    edited

--- CodeWithHarry/practice_set10.java
+++ CodeWithHarry/practice_set10.java
@@ -1,19 +1,24 @@
 package CodeWithHarry;
-class Circle{
-    Circle(int r){
+class Circle1{
+    public  int r;
+    Circle1(int r){
+
         this.r=r;
+        System.out.println(area());
     }
-  public  int r;
+
   public double area(){
       return Math.PI*this.r*this.r;
   }
+
 }
-class Cylinder extends Circle{
-    Cylinder(int r,int h){
+class Cylinder1 extends Circle1{
+    public  int height;
+    Cylinder1(int r,int h){
         super(r);
         this.height=h;
     }
-    public  int height;
+
     public double volume(){
         return Math.PI*this.r*this.r*this.height;
     }
@@ -21,7 +26,7 @@ class Cylinder extends Circle{
 public class practice_set10 {
     public static void main(String[] args) {
 //        Circle objc =new Circle(4);
-        Cylinder obj =new Cylinder(4,5);
-//        System.out.println(obj.volume());
+        Cylinder1 obj =new Cylinder1(4,5);
+        System.out.println(obj.volume());
     }
 }
